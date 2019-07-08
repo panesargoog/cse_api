2267a727863455ad1fb05d0b2163c014150557be
panesar <panesar@google.com>
2019-07-08 11:08:13 -0700

    Add itemId list to response.

--- main.py
+++ main.py
@@ -26,11 +26,9 @@ from itertools import repeat
 app = Flask(__name__)
 service = build("customsearch", "v1", developerKey=api_key)
 
-def search_result_page(search_term, api_key, cse_id, **kwargs):
-    # service = build("customsearch", "v1", developerKey=api_key)
-    search_results = service.cse().siterestrict().list(
-        q=search_term, cx=cse_id, **kwargs).execute()
-    return render_template('index.html', result=search_results)
+
+
+
 
 @app.route('/')
 def form():
@@ -43,7 +41,7 @@ def single_search(q, start):
         sr = service.cse().siterestrict().list(q=q, cx=cse_id, num=10, start=start).execute()
     except googleapiclient.errors.HttpError as err:
         print("Error:", start, err)
-        return
+        return err
 
     t2 = time.time()
     print(q, start, t2 - t1)
@@ -53,35 +51,55 @@ def parallel_search(q):
     t1 = time.time()
     p = Pool(3)
 
-    start = [1,11,21]
+    start = [1, 11, 21]
     results = p.starmap(single_search, zip(repeat(q), start))
     i = 0
     all_results= {}
-    for r in results:
-        if r:
-            if i == 0:
-                all_results = r
-            else:
-                all_results['items'] = all_results['items'] + r['items']
-        i += 1
+    try:
+        for r in results:
+            if r:
+                if i == 0:
+                    all_results = r
+                else:
+                    all_results['items'] = all_results['items'] + r['items']
+            i += 1
+    except IndexError as err:
+        print("Error: ", err)
+        return err
 
     t2 = time.time()
     print(t2 - t1)
     return all_results
 
 
-@app.route('/api')
-def query():
-    q = request.args.get('q')
-    search_results = parallel_search(q)
-
+def construct_item_list(search_results):
+    """From CSE response, return an item list."""
     items = []
     for i in search_results["items"]:
         link = i["link"]
         if "https://www.homedepot.com/p/" in link:
-            pid = link.rsplit('/', 1)[1]
-            items.append(pid)
-    search_results['item_list'] = items
+            try:
+                pid = link.rsplit('/', 1)[1]
+            except IndexError:
+                continue
+            #TODO: Lookup storeId from inventory.
+            item = {
+                'productId': pid,
+                'itemId': pid,
+                'metadata': {
+                    'storeId': 32
+                }
+
+            }
+            items.append(item)
+    return items
+
+
+@app.route('/api')
+def query():
+    q = request.args.get('q')
+    search_results = parallel_search(q)
+    search_results['itemIds'] = construct_item_list(search_results)
 
     return jsonify(search_results)
 
@@ -93,7 +111,25 @@ def result_page():
     """Return the formatted search result page."""
     return results
 
+# Test code follows:
 
+@app.route('/sr')
+def search_result_page():
+    search_term = request.args.get('q')
+    search_results = service.cse().siterestrict().list(
+        q=search_term, cx=cse_id).execute()
+    return render_template('index.html', result=search_results)
+
+
+@app.route('/test')
+def test():
+    q = request.args.get('q')
+    try:
+        sr = service.cse().siterestrict().list(q=q, cx=cse_id, num=10, start=1).execute()
+    except googleapiclient.errors.HttpError as err:
+        print("Error: ",  err)
+        return "Error"
+    return jsonify(sr)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
